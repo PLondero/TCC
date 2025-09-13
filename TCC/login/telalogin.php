29f6d38bfd5d679ffe7b3a92e1b4c1b176ba305f
Plondero <pedroslondero6@gmail.com>
2025-09-12 22:26:28 -0300

    mudando todo o site

---
+++ TCC/login/telalogin.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Educação Financeira</title>
+        <link rel="stylesheet" href="pagprin.css">
+        <link rel="stylesheet" href="extras.css">
+
+
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+
+        
+    </head>
+    <body>
+        <div class="container">
+            <header class="headertopo"> 
+                <div class="titulo1">
+                    <div class="imglogo">
+                        <img class="educa" src="..\imgeral\EDUCA (50 x 50 px).png">
+                    </div>
+                    <h1> EDUCAMONEY</h1>
+                </div>
+                <div class="botheader">
+                    <a href="..\Pagprincipal\pagprincipal.html">
+                        <button> Home </button>
+                        </a>
+                        <a href="">
+                        <button> Entrar </button>
+                        </a>
+                </div>
+            </header>
+
+            <header class="headerbaixo">
+            </header>
